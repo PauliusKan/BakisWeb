d0a70f217d22693949fe1a093195007e4f2f06af
PauliusKan <paulius.kandr@gmail.com>
2023-03-04 20:44:41 +0200

    Added basic map functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { Flex, Box } from "@chakra-ui/react";
+import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
+import { useEffect, useState } from "react";
+
+const markers = [
+  { id: 1, lat: 48.8584, lng: 2.2945 },
+  { id: 2, lat: 48, lng: 2 },
+];
 
 function App() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
+
+  const [center, setCenter] = useState({lat: 48.8584, lng: 2.2945});
+
+  useEffect(() => {
+    navigator?.geolocation.getCurrentPosition(
+      ({ coords: { latitude: lat, longitude: lng } }) => {
+        const pos = { lat, lng };
+        setCenter(pos);
+      }
+    );
+  }, []);
+
+  if (!isLoaded) {
+    return <div>Map isn't loaded</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Flex
+      position="relative"
+      flexDirection="column"
+      alignItems="center"
+      h="100vh"
+    >
+      <Box position="absolute" left={0} right={0} h="100%" w="100%">
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: "100%", height: "100%" }}
+          options={{
+            streetViewControl: false,
+            fullscreenControl: false,
+            styles: [
+              {
+                elementType: "labels",
+                featureType: "transit",
+                stylers: [{ visibility: "off" }],
+              },
+              {
+                elementType: "labels",
+                featureType: "poi",
+                stylers: [{ visibility: "off" }],
+              },
+            ],
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {markers.map((marker) => {
+            return (
+              <MarkerF
+                key={marker.id}
+                position={{ lat: marker.lat, lng: marker.lng }}
+              ></MarkerF>
+            );
+          })}
+        </GoogleMap>
+      </Box>
+    </Flex>
   );
 }
 
